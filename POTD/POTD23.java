bad07e6338879244e403c327e1ae11c6a6d5764e
Vardhan Adheli <vardhanadheli1527@gmail.com>
2025-01-23 20:11:21 +0530

    POTD Update 23-01-2025

---
+++ POTD/POTD23.java
@@ -0,0 +1,31 @@
+class POTD23 {
+    public int countServers(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+        
+        
+        int totalServers = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
+                    totalServers++;
+                }
+            }
+        }
+        
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1) {
+                    totalServers--;
+                }
+            }
+        }
+        
+        return totalServers;
+    }
+}
